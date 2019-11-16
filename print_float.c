3b2cc4ceaaedf158b315f5a26724c99adecaa729
Johnny Webber <jwebber@ox-u3.21-school.ru>
2019-11-16 15:23:59 +0300

    norminated done

--- print_float.c
+++ print_float.c
@@ -1,20 +1,27 @@
-//
-// Created by reoil on 07.11.2019.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_float.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwebber <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/16 15:15:51 by jwebber           #+#    #+#             */
+/*   Updated: 2019/11/16 15:23:23 by jwebber          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
 
-char	*float_leading(long double d)
+char			*float_leading(long double d)
 {
 	return (ft_itoa_base((long long)d, 10));
 }
 
-char 	*float_fraction(long double d, int *precision)
+char			*float_fraction(long double d, int *precision)
 {
 	char	*ret;
 	double	accur;
-	int 	i;
+	int		i;
 
 	*precision = *precision < 0 ? 6 : *precision;
 	if (!(ret = ft_strnew(*precision + (precision != 0))))
@@ -25,12 +32,10 @@ char 	*float_fraction(long double d, int *precision)
 	d -= (long long)d;
 	accur = 5;
 	i = 0;
-	while (i < *precision + 1)
-	{
+	while (i++ < *precision + 1)
 		accur /= 10;
-		i++;
-	}
-	ret[i = 0] = '.';
+	i = 0;
+	ret[0] = '.';
 	d += accur;
 	while (i < *precision)
 	{
@@ -40,7 +45,7 @@ char 	*float_fraction(long double d, int *precision)
 	return (ret);
 }
 
-size_t 	putchar_count(char c, size_t count)
+size_t			putchar_count(char c, size_t count)
 {
 	char	*p;
 
@@ -52,26 +57,11 @@ size_t 	putchar_count(char c, size_t count)
 	return (count);
 }
 
-int				print_float(t_format format, va_list args)
+int				print_float2(t_format format, char *leading, char *fraction)
 {
-	long double	d;
-	char	*leading;
-	char 	*fraction;
-	int 	size;
+	int	size;
 
-	if (format.modifier & MOD_LD)
-		d  = va_arg(args, long double);
-	else
-		d  = va_arg(args, double);
-	size = 0;
-	if(!(leading = float_leading(d)))
-		return (0);
-	if (!(fraction = float_fraction(d, &format.precision)))
-	{
-		ft_strdel(&leading);
-		return (0);
-	}
-	size += ft_strlen(leading) + ft_strlen(fraction);
+	size = ft_strlen(leading) + ft_strlen(fraction);
 	if (format.flags & FLAG_PLUS)
 	{
 		size++;
@@ -86,4 +76,24 @@ int				print_float(t_format format, va_list args)
 	ft_strdel(&leading);
 	ft_strdel(&fraction);
 	return (size);
+}
+
+int				print_float(t_format format, va_list args)
+{
+	long double	d;
+	char		*leading;
+	char		*fraction;
+
+	if (format.modifier & MOD_LD)
+		d = va_arg(args, long double);
+	else
+		d = va_arg(args, double);
+	if (!(leading = float_leading(d)))
+		return (0);
+	if (!(fraction = float_fraction(d, &format.precision)))
+	{
+		ft_strdel(&leading);
+		return (0);
+	}
+	return (print_float2(format, leading, fraction));
 }
