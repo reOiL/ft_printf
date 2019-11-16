42c05590e3c4a2dc877e2d790dde45454a2b3813
Johnny Webber <jwebber@ox-u3.21-school.ru>
2019-11-16 14:47:41 +0300

    Added first construction

--- color.h
+++ color.h
@@ -13,5 +13,7 @@
 # define BC_TEXT_YELLOW 33
 # define BC_TEXT_BLUE 34
 # define BC_TEXT_MAGNET 35
+# define BC_TEXT_CYAN 36
 
+char	*code_to_color(char *code);
 #endif //FT_PRINTF_COLOR_H
--- colors.c
+++ colors.c
@@ -2,3 +2,16 @@
 // Created by Johnny Webber on 16/11/2019.
 //
 
+#include "color.h"
+#include "libft/libft.h"
+
+char	*code_to_color(char *code)
+{
+	if (ft_strncmp(code, "{red}", 5))
+		return "\e[31m";
+	if (ft_strncmp(code, "{green}", 7))
+		return "\e[32m";
+	if (ft_strncmp(code, "{blue}", 6))
+		return "\e[34m";
+	return "";
+}
